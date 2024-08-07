67e90356069c5cce5c4d7a42c0ad8055b9fb29df
Your name <Your email>
2024-08-07 21:47:53 +0200

    loading

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,16 @@
+import React, { useRef, useEffect, useState } from "react"; // Ajoutez useState
 import styles from "../styles/Home.module.css";
 import Header from "./Header";
 import Section1 from "./Section1";
 import Shop from "./Shop";
 import Footer from "./Footer";
-import React, { useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 
 function Home() {
   const acceuilRef = useRef(null);
   const shopRef = useRef(null);
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true); // Ajoutez cet état
 
   useEffect(() => {
     if (router.query.scrollTo === "shop" && shopRef.current) {
@@ -17,11 +18,41 @@ function Home() {
     }
   }, [router.query]);
 
+  useEffect(() => {
+    const images = document.querySelectorAll("img");
+    let imagesLoaded = 0;
+
+    images.forEach((img) => {
+      if (img.complete) {
+        imagesLoaded++;
+      } else {
+        img.addEventListener("load", () => {
+          imagesLoaded++;
+          if (imagesLoaded === images.length) {
+            setIsLoading(false);
+          }
+        });
+      }
+    });
+
+    if (imagesLoaded === images.length) {
+      setIsLoading(false);
+    }
+  }, []);
+
   const scrollToSection = (ref) => {
     if (ref.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader"></div>
+      </div>
+    );
+  }
 
   return (
     <div className="scrollbar-hide">
